e88f2b8c5f06cc22d15323c884386440ba45ac4a
Siddhesh Desai <desaisiddhesh1@gmail.com>
2022-03-14 22:55:31 +0530

    Added Project

---
+++ name.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    string nm, sn;
+    cout << "Print name and Surname";
+    cin >> nm >> sn;
+    cout << "Name is" << nm << " " << sn;
+    return 0;
+}
